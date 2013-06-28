745ac8f8d586db32be1b867072d2e9ef2faffbb1
Manivannan J <jmanivannancse@gmail.com>
2013-06-28 19:40:37 +0530

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MultipleDb306::Application.config.secret_token = '773b7ca2883066798cc3db32ccedf5fdd69d41a4f54dbb1db02b9eb22e7a49e4e9c5d5676903c23d73bced03660418e8adf5cbd57e1638932531e5c319355bda'
+MultipleDb306::Application.config.secret_token = 'secret_token'
